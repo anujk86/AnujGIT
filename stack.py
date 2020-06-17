9ca1277fcd668807e2271c8217fe9684dc3baea0
anujk86 <60627254+anujk86@users.noreply.github.com>
2020-06-17 23:10:23 +0200

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,33 @@
+import ssl
+
+stack = []
+def push(item):
+	stack.append(item)
+	print('Item added in stack:', item)
+
+def pop():
+	if (isempty() == False):
+		print('Stack is empty !!')
+		return
+	print('Removed Item :',stack.pop())
+def top():
+	if (isempty() == False):
+		print('Stack is empty !!')
+		return
+	print('Top Item from Stack :',stack [len(stack)-1])
+
+def isempty():
+	if len(stack)> 0:
+		return True
+	else:
+		return False
+
+push(3)
+push(4)
+push(5)
+top()
+pop()
+top()
+pop()
+pop()
+pop()
