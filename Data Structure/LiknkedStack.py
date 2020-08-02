0e4aaba1adbe1e50baba80fa276d32a4bc1c4820
anujk86 <60627254+anujk86@users.noreply.github.com>
2020-08-02 18:50:42 +0200

    Add files via upload

--- Data Structure/LiknkedStack.py
+++ Data Structure/LiknkedStack.py
@@ -1,26 +1,20 @@
 class Node: 
       
-    # Class to create nodes of linked list 
-    # constructor initializes node automatically 
     def __init__(self,data): 
         self.data = data 
         self.next = None
       
 class Stack: 
       
-    # head is default NULL 
     def __init__(self): 
         self.head = None
       
-    # Checks if stack is empty 
     def isempty(self): 
         if self.head == None: 
             return True
         else: 
             return False
       
-    # Method to add data to the stack 
-    # adds to the start of the stack 
     def push(self,data): 
           
         if self.head == None: 
@@ -30,22 +24,18 @@ class Stack:
             newnode = Node(data) 
             newnode.next = self.head 
             self.head = newnode 
-      
-    # Remove element that is the current head (start of the stack) 
+       
     def pop(self): 
           
         if self.isempty(): 
             return None
               
         else: 
-            # Removes the head node and makes  
-            #the preceeding one the new head 
             poppednode = self.head 
             self.head = self.head.next
             poppednode.next = None
             return poppednode.data 
       
-    # Returns the head node data 
     def peek(self): 
           
         if self.isempty(): 
--- Data Structure/LinkedListQueue.py
+++ Data Structure/LinkedListQueue.py
@@ -3,11 +3,6 @@ class Node:
     def __init__(self, data): 
         self.data = data 
         self.next = None
-  
-# A class to represent a queue 
-  
-# The queue, front stores the front node 
-# of LL and rear stores the last node of LL 
 class Queue: 
       
     def __init__(self): 
@@ -15,8 +10,7 @@ class Queue:
   
     def isEmpty(self): 
         return self.front == None
-      
-    # Method to add an item to the queue 
+       
     def EnQueue(self, item): 
         temp = Node(item) 
           
@@ -26,7 +20,6 @@ class Queue:
         self.rear.next = temp 
         self.rear = temp 
   
-    # Method to remove an item from queue 
     def DeQueue(self): 
           
         if self.isEmpty(): 
@@ -36,8 +29,7 @@ class Queue:
   
         if(self.front == None): 
             self.rear = None
-  
-# Driver Code 
+
 if __name__== '__main__': 
     q = Queue() 
     q.EnQueue(10) 
