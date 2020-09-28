91a4e60e9a9328a504befbec9eb9c4a146eeef3b
anujk86 <60627254+anujk86@users.noreply.github.com>
2020-09-28 15:19:04 +0200

    Add files via upload

---
+++ Data Structure/DoublyLinkedList.py
@@ -0,0 +1,170 @@
+import sys
+
+"""
+Homework (Please Exeute in the same order ): 
+
+SINGLY LINKED LIST 
+
+-> Functions to write 
+1) add_elememt_at_the start 
+2) Delete element at the start 
+3) is_empty
+4) add element at the end 
+5) delete element at the end 
+6) add element at the kth Location of a linked List                           
+7) Delete Element at the kth location of a linked List 
+8) Traverse the linked List 
+9) Reverse the Linked List  
+"""
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("list is not empty")
+
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+
+    def delete_at_start(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        self.start_node = self.start_node.nref
+        self.start_prev = None;
+
+    def delete_at_end(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        n.pref.nref = None
+
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.nref
+
+    def insert_after_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.pref = n
+                new_node.nref = n.nref
+                if n.nref is not None:
+                    n.nref.prev = new_node
+                n.nref = new_node
+
+    def delete_element_at_Specific(self, x):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                self.start_node = None
+            else:
+                print("Item not found")
+            return 
+
+        if self.start_node.item == x:
+            self.start_node = self.start_node.nref
+            self.start_node.pref = None
+            return
+
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                break;
+            n = n.nref
+        if n.nref is not None:
+            n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                n.pref.nref = None
+            else:
+                print("Element not found")
+
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.nref
+
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        p = self.start_node
+        q = p.nref
+        p.nref = None
+        p.pref = q
+        while q is not None:
+            q.pref = q.nref
+            q.nref = p
+            p = q
+            q = q.pref
+        self.start_node = p
+
+new_linked_list = DoublyLinkedList()
+new_linked_list.insert_at_start(10)
+new_linked_list.insert_at_start(5)
+new_linked_list.insert_at_end(18)
+new_linked_list.traverse_list()
